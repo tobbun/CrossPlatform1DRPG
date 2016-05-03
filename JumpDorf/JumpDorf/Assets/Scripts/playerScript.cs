4ca1c5492970b7d228e6ff36b2b49c4cf3d4ad22
tobbun <tobbun@gmail.com>
2016-05-04 00:18:23 +0200

    Continued
    Added basic animation (switches between the 3 sprites based on vertical
    velocity).
    Threw some music files in a folder for good measure.
    Did fuck-all else.
    Went to bed.

--- JumpDorf/JumpDorf/Assets/Scripts/playerScript.cs
+++ JumpDorf/JumpDorf/Assets/Scripts/playerScript.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class playerScript : MonoBehaviour {
 
+    public Animator anim;
+    SpriteRenderer spr;
+    public int jumpSpeed, steeringSpeed;
+    public float currentYSpeed;
+    bool freeJump;
+    public bool living = true;
+    Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-	
+        rb = GetComponent<Rigidbody2D>();
+        spr = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        currentYSpeed = rb.velocity.y;
+        anim.SetFloat("Y Speed", currentYSpeed);
+        
+        if (freeJump == true){
+            YouCanJumpInTheAirNow();
+        }
+
 	}
+
+    void YouCanJumpInTheAirNow()
+    {
+        if(Input.GetKey(KeyCode.UpArrow)){
+
+        }
+    }
 }
