0158c8f47bcfdab4e1e667ff5bf3a2f82e65fb72
tobbun <tobbun@gmail.com>
2016-05-05 09:41:49 +0200

    Added 'jump', before work.
    Added jumping through Velocity. It only triggers if the bool "canJump"
    is true.
    
    Having some problems with bouncing (core loop mechanic) because there's
    some collision logic I'm not understanding. Will work on this later
    today.

--- JumpDorf/JumpDorf/Assets/Scripts/playerScript.cs
+++ JumpDorf/JumpDorf/Assets/Scripts/playerScript.cs
@@ -7,17 +7,18 @@ public class playerScript : MonoBehaviour {
     SpriteRenderer spr;
     public int jumpSpeed, steeringSpeed;
     public float currentYSpeed;
-    bool freeJump;
+    public bool freeJump;
     public bool living = true;
     Rigidbody2D rb;
 
-	// Use this for initialization
+	// adds rigidbody and spriterenderer, 
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
         spr = GetComponent<SpriteRenderer>();
+        rb.velocity = new Vector2();
 	}
 	
-	// Update is called once per frame
+	
 	void FixedUpdate () {
         currentYSpeed = rb.velocity.y;
         anim.SetFloat("Y Speed", currentYSpeed);
@@ -30,8 +31,27 @@ public class playerScript : MonoBehaviour {
 
     void YouCanJumpInTheAirNow()
     {
-        if(Input.GetKey(KeyCode.UpArrow)){
-
+        if(Input.GetKey(KeyCode.UpArrow)||Input.GetKey(KeyCode.W)){
+            Jump();
         }
+    }
+
+    void OnCollisionEnter(Collision colin)
+    {
+        Debug.Log("You've just touched: " + colin.gameObject.tag);
+        if (colin.gameObject.tag == "Head")
+        {
+            Jump();
+        }
+        else if (colin.gameObject.tag == "Sharp")
+        {
+            living = false;
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(0, 10);
     }
 }
